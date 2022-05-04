830f5a63fe4542f5d1b8c2770d1e5531ee99db64
Angelo Fernandes Oliveira <afbo2003@gmail.com>
2022-05-04 11:59:44 -0300

    feat(front) cria steps do feedback, animacoes do form e estilizacao com tailwind

---
+++ nlw-return-web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+
+import { FeddbackTypeStep } from './Steps/FeedbackTypeStep';
+
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/idea.svg';
+import thoughtImageUrl from '../../assets/thought.svg';
+import { FeedBackContentStep } from './Steps/FeedbackContentStep';
+import { FeedbackSuccessStep } from './Steps/FeedbackSuccessStep';
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Imagem de um inseto',
+    },
+  },
+  IDEA: {
+    title: 'Idéia',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Imagem de uma lâmpada',
+    },
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: thoughtImageUrl,
+      alt: 'Imagem de uma nuvem de pensamento',
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSend, setFeedbackSend] = useState(false);
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+    setFeedbackSend(false);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] sm:w-auto">
+      {feedbackSend ? (
+        <FeedbackSuccessStep onFeedbackRestart={handleRestartFeedback} />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeddbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedBackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestart={handleRestartFeedback}
+              onFeedbackSend={() => setFeedbackSend(true)}
+            />
+          )}
+        </>
+      )}
+
+      <footer className="text-xs text-neutral-400">
+        Feito com 💙 por Angelo Fernandes
+      </footer>
+    </div>
+  );
+}
