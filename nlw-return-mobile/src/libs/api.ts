2181ea21b4a615cbee15a8a180d6d105d7312b25
Angelo Fernandes Oliveira <afbo2003@gmail.com>
2022-05-10 16:17:30 -0300

    feat(api) implementa env para PORT da api

--- nlw-return-mobile/src/libs/api.ts
+++ nlw-return-mobile/src/libs/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "http://172.26.202.165:3001"
+  baseURL: "http://172.22.0.1:3001"
   // baseURL: "http://localhost:3001"
 })
